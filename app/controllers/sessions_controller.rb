fda14c3361ff3cdbd93bb9c4c34d8e4e04ad83c0
norah ngatjizeko <tjeripo86@live.com>
2021-06-13 17:32:05 +0900

    login update

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,19 +1,20 @@
 class SessionsController < ApplicationController
+  before_action :login_required, :except=>[:new, :create]
   def new
   end
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      session[:user_id] = user.id
-      redirect_to user_path(user.id)
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user && @user.authenticate(params[:session][:password])
+      session[:user_id] = @user.id
+      redirect_to user_path(@user.id)
     else
-      flash[:danger] = 'I failed to login'
+      flash.now[:danger] = 'Login failed'
       render :new
     end
     def destroy
-      session.delete(:user_id)
-      flash[:notice] = 'logged out'
-      redirect_to new_session_path
+    session.delete(:user_id)
+    flash[:notice] = 'logged out'
+    redirect_to new_session_path
   end
   end
 end
