f74de9e3ca1bdc7f06969245fbc84ed3cf9b523d
norah ngatjizeko <tjeripo86@live.com>
2021-06-14 16:10:24 +0900

    update

--- Gemfile
+++ Gemfile
@@ -24,7 +24,9 @@ gem 'jbuilder', '~> 2.7'
 gem 'bcrypt'
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
-
+gem 'carrierwave'
+gem 'mini_magick'
+gem 'mimemagic', '~> 0.4.3'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.4', require: false
 
--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,28 +1,35 @@
 class FeedsController < ApplicationController
   before_action :set_feed, only: %i[ show edit update destroy ]
 
-  # GET /feeds or /feeds.json
   def index
     @feeds = Feed.all
   end
 
-  # GET /feeds/1 or /feeds/1.json
   def show
   end
 
-  # GET /feeds/new
   def new
-    @feed = Feed.new
+    if params[:back]
+      @feed = Feed.new
+    else
+      @feed = Feed.new
+    end
   end
 
-  # GET /feeds/1/edit
   def edit
   end
 
-  # POST /feeds or /feeds.json
   def create
-    @feed = Feed.new(feed_params)
-
+    @feed = current_user.feeds.build(feed_params)
+    if params[:back]
+      render :new
+    else
+      if @feed.save
+        redirect_to @feed, notice: "Feed was Posted"
+      else
+        render :new
+      end
+    end
     respond_to do |format|
       if @feed.save
         format.html { redirect_to @feed, notice: "Feed was successfully created." }
@@ -34,6 +41,10 @@ class FeedsController < ApplicationController
     end
   end
 
+  def confirm
+    @feed = current_user.feeds.build(picture_params)
+    render :new if @feed.invalid?
+  end
   # PATCH/PUT /feeds/1 or /feeds/1.json
   def update
     respond_to do |format|
@@ -64,6 +75,12 @@ class FeedsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def feed_params
-      params.require(:feed).permit(:image)
+      params.require(:feed).permit(:content, :image, :image_cache)
+    end
+
+    def user_login_check
+      unless logged_in?
+        redirect_to root_path
+      end
     end
 end
